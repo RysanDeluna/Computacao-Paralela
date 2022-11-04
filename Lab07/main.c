456c3be0d3c46f6150253f16dc45efb09a234f16
RysanDeluna <82891214+RysanDeluna@users.noreply.github.com>
2022-11-04 13:18:33 -0300

    Update main.c

--- Lab07/main.c
+++ Lab07/main.c
@@ -6,6 +6,7 @@
 
 void Trap (double a, double b, int n, double* global_result_p);
 double f (double x);
+void troca (double* a_p, double* b_p);
 
 int main(int argc, char* argv[]){
     double global_result = 0.0;
@@ -16,6 +17,7 @@ int main(int argc, char* argv[]){
     thread_count = strtol(argv[1], NULL, 10);
     printf("Enter a, b, and n\n");
     scanf("%lf %lf %d", &a, &b, &n);
+    if (a > b) troca(&a, &b);
   #  pragma omp parallel num_threads(thread_count)
     Trap (a, b, n, &global_result);
 
@@ -53,3 +55,10 @@ void Trap (double a, double b, int n, double* global_result_p){
 }
 
 double f (double x) { return cos(pow(e, -x)); }
+
+void troca (double * a_p, double * b_p) 
+{
+    int temp = *a_p;
+    *a_p = *b_p;
+    *b_p = temp;
+}

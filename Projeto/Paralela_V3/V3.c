6e86bb756cc7c4c25d675ea6f15427f6dec2b369
joy <74800062+joy0119@users.noreply.github.com>
2022-11-09 14:51:30 -0300

    Update V3.c

--- Projeto/Paralela_V3/V3.c
+++ Projeto/Paralela_V3/V3.c
@@ -1,52 +1,43 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h> 
 
-long long int      	pedaco;
-int                	QNTD_THREADS;
+unsigned long long int fat(int n);
+void taylor(int n, long double *global);
 
+long long int fats[1000000] = {0};
 
-long long int fats[1000000];
-
-unsigned long long int fat(int n) 
-{
-    if (n == 0) fats[n] = 1;
-    else fats[n] = n * fat(n-1);
-	//if (fats[n] < 0) fats[n] *= -1;
-	return fats[n];
+unsigned long long int fat(int n) {
+  if (n == 0)
+    fats[n] = 1;
+  else
+    fats[n] = n * fat(n - 1);
+  return fats[n];
 }
 
-void taylor(long double* global_result_p){
-    int my_rank = omp_get_thread_num();
-    int thread_count = omp_get_num_threads();
+void taylor(int n, long double *global, omp_lock_t trava) {
+  long double my_result = 0.0;
+  int minha_thread = omp_get_thread_num();
+  int thread_count = omp_get_num_threads();
 
-    long long my_pedaco = pedaco /thread_count;
-    long long my_first_i = my_pedaco * my_rank;
-    long long my_last_i = my_first_i + my_pedaco;
-    long double my_tysum;
-    int i;
+  for (int i = minha_thread; i < n; i += thread_count)
+    my_result += 1 / (double)fat(i);
 
-     for (i = my_first_i; i < my_last_i; i++) my_tysum += 1 / (double) fats[i];
-
-#    pragma omp critical
-    *global_result_p += my_tysum;
+#pragma omp critical
+  *global += my_result;
 }
 
+int main(int argc, char *argv[]) {
+  int thread_count;
+  long double global;
 
-int main(int argc, char *argv[])
-{
-    long double global_result = 0.0;
-    int n;
-    
-    QNTD_THREADS = strtol(argv[1], NULL, 10);
-    pedaco = strtol(argv[2], NULL, 10);
+  thread_count = strtol(argv[1], NULL, 10);
 
-    fat(pedaco);
+  fat(50);
 
- #  pragma omp parallel num_threads(QNTD_THREADS)
-    taylor(&global_result);
+#pragma omp parallel num_threads(thread_count)
+  long double taylor(50, &global, trava);
 
-    printf("resultado: %.33Lf \n", global_result);
-    
+  printf("O resultado obtido é: %.33Lf \n", global);
+  return 0;
 }
- 
